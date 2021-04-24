0308da1dd07e39499f782a82ec80e053db754244
Piia Jouttela <piia.jouttela@gmail.com>
2021-04-24 14:59:49 +0300

    change index.html and new code in script.js

--- script.js
+++ script.js
@@ -1,29 +1,81 @@
-document.getElementById('nappi').addEventListener('click', getData());
+document.getElementById('bottone').addEventListener('click', getAllData);
+document.getElementById('Cronache').addEventListener('click', getColumns);
 
-function getData() {
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET","http://xml2.corriereobjects.it/rss/homepage.xml",true);
-xmlhttp.send();
 
-xmlhttp.onreadystatechange=function() {
-    if (xmlhttp.readyState==4 && xmlhttp.status==200){
-        console.log ( xmlhttp.responseText);
+function getAllData() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET","http://xml2.corriereobjects.it/rss/homepage.xml",true);
+    xmlhttp.send();
+
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200){
+            console.log ( xmlhttp.responseText);
         
-        // tallennetaan xml muuttujaan
-        var data = xmlhttp.responseXML;
+            // tallennetaan xml muuttujaan
+            var data = xmlhttp.responseXML;
 
-        // tallennetaan title-, creator- ja link-tägit
-        var titles = data.getElementsByTagName('title');
-        var links = data.getElementsByTagName('link');
-        var writers = data.getElementsByTagName('dc:creator');
+            // tallennetaan title-, creator- ja link-tägit
+            var titles = data.getElementsByTagName('title');
+            var links = data.getElementsByTagName('link');
+            var published = data.getElementsByTagName('pubDate');
 
+            // tarkistetaan, onko uutisotsikkolista tyhjä, jos ei, tyhjennetään
+            var listing = document.getElementById('showList');
 
-        // taulukot käydään läpi
-        for (var i=2; i < titles.length; i++){
-            var testi = '<a href=' + links[i].innerHTML + '>' + titles[i].innerHTML + '</a>' 
-            console.log(testi);
-            document.write('<strong>'+testi+'</strong> ('+writers[i].innerHTML+')<br>');
+            emptyList(listing);
+
+            // taulukot käydään läpi, titlet alkavat indeksistä 2, julkaisuajat indeksistä 0
+            for (var i=2; i < titles.length; i++){
+                var titleWithLink = '<a href=' + links[i].innerHTML + '>' + titles[i].innerHTML + '</a>';
+
+                listing.innerHTML += '<li><strong>' + titleWithLink + '</strong> (' + published[i-2].innerHTML + ')</li>';
+            }
         }
     }
 }
+
+function getColumns() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET","http://xml2.corriereobjects.it/rss/homepage.xml",true);
+    xmlhttp.send();
+
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200){
+            console.log ( xmlhttp.responseText);
+        
+            // tallennetaan xml muuttujaan
+            var data = xmlhttp.responseXML;
+
+            // tallennetaan title- ja link-tägit data-muuttujasta
+            var titles = data.getElementsByTagName('title');
+            var links = data.getElementsByTagName('link');
+            var categories = data.getElementsByTagName('category');
+
+            // tarkistetaan, onko uutisotsikkolista tyhjä, jos ei, tyhjennetään
+            var listing = document.getElementById('showList');
+             
+            // lista tyhjennetään
+            emptyList(listing);
+            addToList('Cronache');
+
+            
+            // taulukot käydään läpi, titlet ja listit alkavat indeksistä 2, kategoriat indeksistä 0
+            for (var i=2; i < titles.length; i++){
+                if(categories[i-2].innerHTML == 'Cronache') {
+                    var titleWithLink = '<a href=' + links[i].innerHTML + '>' + titles[i].innerHTML + '</a>';
+                    listing.innerHTML += '<li>' + titleWithLink + '</li>';
+                }
+            }
+        }
+    }
 }
+
+
+// lapsien poistaminen, lista tyhjenee
+function emptyList(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
